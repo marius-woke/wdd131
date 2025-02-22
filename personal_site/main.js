f692888e4540f24ee1e70ab74a7a2cd876e07c4a
marius-woke <wok22002@byui.edu>
2025-02-22 00:54:04 -0700

    final project

---
+++ personal_site/main.js
@@ -0,0 +1,25 @@
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1]) // here the length of items = 6
+})
+function toggleMenu() {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('active');
+}
+
+window.addEventListener('resize', () => {
+    const nav = document.querySelector('nav');
+    if (window.innerWidth > 768) {
+        nav.classList.add('active');
+    } else {
+        nav.classList.remove('active');
+    }
+});
